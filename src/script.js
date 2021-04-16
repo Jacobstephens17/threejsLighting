c6f17ca772b70e2e633db227d30d93699cfacb0b
Jacobstephens17 <64627086+Jacobstephens17@users.noreply.github.com>
2021-04-15 20:58:06 -0700

    added house with window and door, with some bushes

--- src/script.js
+++ src/script.js
@@ -23,13 +23,44 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * House
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+// GROUP
+const house = new THREE.Group()
+scene.add(house)
+
+
+// WALLS
+const walls = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(10.0,2,5),
+    new THREE.MeshStandardMaterial({color: 'brown'})
+    
 )
-sphere.position.y = 1
-scene.add(sphere)
+
+walls.position.x = 0
+walls.position.y = 2 / 2
+walls.position.z = -1.5
+house.add(walls)
+
+
+// ROOF
+const roof = new THREE.Mesh(
+    new THREE.ConeBufferGeometry(6,2,6),
+    new THREE.MeshStandardMaterial({color:'red'})
+)
+// house.add(roof)
+// roof.position.y = 3.25
+// roof.position.z = -1.4
+// roof.rotation.y = 150
+
+// Window 
+const windows = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(9.5,1.8),
+    new THREE.MeshStandardMaterial({color:'black'})
+)
+windows.position.z = 1.01
+windows.position.y = 1
+windows.position.x = 0
+windows.rotation.z = 0
+house.add(windows)
 
 // Floor
 const floor = new THREE.Mesh(
@@ -39,6 +70,52 @@ const floor = new THREE.Mesh(
 floor.rotation.x = - Math.PI * 0.5
 floor.position.y = 0
 scene.add(floor)
+
+
+// BUSHES
+const bushGeometry = new THREE.SphereBufferGeometry(1,16,16)
+const bushMaterial = new THREE.MeshStandardMaterial({color:'limegreen'})
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+house.add(bush1)
+bush1.scale.set(0.5,0.5,0.5)
+bush1.position.set(5.5,0.3,-3.4)
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+house.add(bush2)
+bush2.scale.set(0.5,0.5,0.5)
+bush2.position.set(5.5,0.3,-2.4)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+house.add(bush3)
+bush3.scale.set(0.5,0.5,0.5)
+bush3.position.set(5.5,0.3,-1.4)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+house.add(bush4)
+bush4.scale.set(0.5,0.5,0.5)
+bush4.position.set(5.5,0.3,-.4)
+
+const bush5 = new THREE.Mesh(bushGeometry, bushMaterial)
+house.add(bush5)
+bush5.scale.set(0.5,0.5,0.5)
+bush5.position.set(5.5,0.3,.6)
+
+
+// Lights
+
+// Door
+const door = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(1.1,1.6),
+    new THREE.MeshStandardMaterial({color:'white'})
+)
+house.add(door)
+door.position.set(-5.05,.85,0)
+door.rotation.y = 4.709
+
+
+
+
 
 /**
  * Lights
